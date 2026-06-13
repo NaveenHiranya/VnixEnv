754a6fc6d8277dfdea6a9d4822653fcf9d0234fa
NaveenHiranya <naveenhiranya999@gmail.com>
2026-06-13 10:53:12 +0530

    feat: logout

---
+++ app/api/logout/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  const response = NextResponse.json({
+    message: "Logged out successfully",
+  });
+
+  response.cookies.set("userId", "", {
+    httpOnly: true,
+    path: "/",
+    maxAge: 0,
+  });
+
+  return response;
+}
